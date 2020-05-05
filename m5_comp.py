cbac6a82ec43668b6c95e543c1664dd0ea2a8a77
paoloangeles <paolo.angeles09@gmail.com>
2020-05-05 18:34:29 +0100

    Initial commit

---
+++ m5_comp.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  4 19:51:01 2020
+
+@author: paoloangeles
+"""
+
+import pandas as pd
+
+calendar = pd.read_csv("calendar.csv")
+sell_prices = pd.read_csv("sell_prices.csv")
