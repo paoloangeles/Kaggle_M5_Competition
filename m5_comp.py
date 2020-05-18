443ad5e0ec47de195cf423245e5e381a8524d9c3
paoloangeles <paolo.angeles09@gmail.com>
2020-05-18 21:03:34 +0100

    Added data into array for quicker plotting
    Also fixed documentation as was incorrect

--- m5_comp.py
+++ m5_comp.py
@@ -9,9 +9,17 @@ Created on Mon May  4 19:51:01 2020
 # Import modules
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # Get data from csvs
 calendar = pd.read_csv("calendar.csv")
 sell_prices = pd.read_csv("sell_prices.csv")
 sales_train = pd.read_csv("sales_train_validation.csv")
+
+number_of_days = 1913
+a = []
+
+for product in range(len(sales_train)):
+    for day in range(number_of_days):
+        a.append(sales_train.at[product, 'd_'+str(day+1)])
