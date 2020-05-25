49594c0d1c3fccd27e4938c4b15f13217c55e560
paoloangeles <paolo.angeles09@gmail.com>
2020-05-25 19:50:19 +0100

    Adjusted period plotting method

--- m5_comp.py
+++ m5_comp.py
@@ -15,14 +15,26 @@ calendar = pd.read_pickle("./calendar.pkl")
 sell_prices = pd.read_pickle("./sell_prices.pkl")
 sales_train_validation = pd.read_pickle("./sales_train_validation.pkl")
 
+## Data preprocessing
+## Check for any NaN values
+total_num_days = 6
+for day in range(total_num_days):
+    null_rows = sales_train_validation["d_" + str(day+1)].isnull().any()
+    print(null_rows)
+
 # Plot visualisation
 
 # Product id
 product_id = 0
 
 # Time constraints
-start_index_sales = 6
-year = 25
-days_in_year = 365
+start_index_sales = 6 # initial columns are not sales numbers
+start_point = 50
 
-sales_train_validation.iloc[product_id, ((year-1)*days_in_year)+start_index_sales:(year*days_in_year)+start_index_sales+1].plot.line()
+period_length = "month"
+if period_length == "month":
+    days = 28
+elif period_length == "year":
+    days = 365
+
+sales_train_validation.iloc[product_id, ((start_point-1)*days)+start_index_sales:(start_point*days)+start_index_sales+1].plot.line()
